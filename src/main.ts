a2e11d3602025b1c3d7dc8d16aa7c66667f2e883
itsrusty <rusty3031@gmail.com>
2024-03-21 11:17:10 -0700

    feat: create a new service & config app

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+  const service = app.get(ConfigService)
+  const PORT = service.get('PORT')
+  app.enableCors()
+
+  await app.listen(PORT);
 }
 bootstrap();
